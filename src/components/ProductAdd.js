6fd657a71a16ef1a10d4a67447e77f6e09c85993
Simran Deol <simrandeol@Simrans-MacBook-Pro.local>
2020-04-20 00:10:46 +0530

    update implemented and redirection done

--- src/components/ProductAdd.js
+++ src/components/ProductAdd.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import {addProduct} from '../actions'; 
+import {Redirect} from "react-router-dom";
 
 class ProductAdd extends Component{
 
@@ -11,70 +12,81 @@ class ProductAdd extends Component{
     state = {
         productName: '', 
         price:'',
-        type:''
+        type:'',
+        refresh: false
     }
 
     handleChange = (e) => {
         this.setState({
-            [e.target.name] : e.target.value
+            [e.target.name] : e.target.value,
         });
     }
 
     handleSubmit = (e) => {
         e.preventDefault();
         this.props.addProduct(this.state);
+        this.setState({
+          refresh: true,
+        });
+        // return(<Redirect to="/products" />);
     }
 
     render()
     {
-    return(
-       <div className="container">
-        <h3>Add Products</h3>
-            <form onSubmit={this.handleSubmit.bind(this)}>
-                  <div className="row">
-                    <input
-                      placeholder="Product Name"
-                      id="productName"
-                      type="text"
-                      name="productName"
-                      value={this.state.productName}
-                      onChange={this.handleChange}
-                    />
-                  </div>
-                  <div className="row">
-                    <input
-                      placeholder="Price"
-                      id="price"
-                      type="text"
-                      name="price"
-                      value={this.state.price}
-                      onChange={this.handleChange}
-                    />
-                  </div>
-                  <div className="row">
-                    <input
-                      placeholder="Type"
-                      id="type"
-                      type="text"
-                      name="type"
-                      value={this.state.type}
-                      onChange={this.handleChange}
-                    />
-                  </div>
-                  <div className="row">
-                    <input type="text" name="id" value={this.state.id} />
-                  </div>
-                  <div className="row">
-                    <button
-                      type="submit"
-                      className="waves-effect waves-light btn right-align"
-                    >
-                    Add  
-                    </button>
-                  </div>
-            </form>
-       </div>
+      if(this.state.refresh){
+        return (<Redirect to="/products"/>)
+      }
+      else{
+          return(
+          <div className="container">
+            <h3>Add Products</h3>
+                <form onSubmit={this.handleSubmit.bind(this)}>
+                      <div className="row">
+                        <input
+                          placeholder="Product Name"
+                          id="productName"
+                          type="text"
+                          name="productName"
+                          value={this.state.productName}
+                          onChange={this.handleChange}
+                        />
+                      </div>
+                      <div className="row">
+                        <input
+                          placeholder="Price"
+                          id="price"
+                          type="text"
+                          name="price"
+                          value={this.state.price}
+                          onChange={this.handleChange}
+                        />
+                      </div>
+                      <div className="row">
+                        <input
+                          placeholder="Type"
+                          id="type"
+                          type="text"
+                          name="type"
+                          value={this.state.type}
+                          onChange={this.handleChange}
+                        />
+                      </div>
+                      <div className="row displayNone">
+                        <input type="text" name="id" value={this.state.id} />
+                      </div>
+                      <div className="row">
+                        <button
+                          type="submit"
+                          className="waves-effect waves-light btn right-align"
+                        >
+                        Add  
+                        </button>
+                      </div>
+                </form>
+          </div>
      );
+      }
+   
     }   
 }
 
--- src/components/ProductEdit.js
+++ src/components/ProductEdit.js
@@ -14,6 +14,7 @@ class ProductEdit extends Component {
       price: product.price !== undefined ? product.price : "",
       type: product.type !== undefined ? product.type : "",
       id: product.id !== undefined ? product.id : "",
+      refresh : false,
     };
 
     this.handleChangeName = this.handleChangeName.bind(this);
@@ -46,76 +47,84 @@ class ProductEdit extends Component {
 
   handleEdit = (e) => {
        this.props.updateProduct(this.state);
-       return (<Redirect to="/products" />);
+    //    return (<Redirect to="/products" />);
   } 
 
   handleSubmit =(e) => {
       e.preventDefault();
-      return (<Redirect to="/products" />);
+      this.setState({
+           refresh: true
+       });
+      
   }
 
   render() {
     const { product } = this.props.location.state;
 
-    if (product !== undefined) {
-      return (
-        <div className="container">
-          <h3>Edit Products</h3>
-          <form onSubmit = {this.handleSubmit.bind(this)}>
-            <div className="row">
-              <input
-                placeholder="Product Name"
-                id="productName"
-                type="text"
-                name="productName"
-                value={this.state.productName}
-                onChange={this.handleChangeName}
-              />
-            </div>
-            <div className="row">
-              <input
-                placeholder="Price"
-                id="price"
-                type="text"
-                name="price"
-                value={this.state.price}
-                onChange={this.handleChangePrice}
-              />
-            </div>
-            <div className="row">
-              <input
-                placeholder="Type"
-                id="type"
-                type="text"
-                name="type"
-                value={this.state.type}
-                onChange={this.handleChangeType}
-              />
-            </div>
-            <div className="row" className="displayNone">
-              <input
-               disabled 
-                placeholder="ID"
-                id="ID"
-                type="text"
-                name="type"
-                value={this.state.id}
-              />
-            </div>
-            <div className="row">
-              <button
-                type="submit"
-                className="waves-effect waves-light btn right-align"
-                onClick = {this.handleEdit}
-              >
-                Add
-              </button>
-            </div>
-          </form>
-        </div>
-      );
-    } else {
-      return <h3>No Item</h3>;
+    if(this.state.refresh){
+        return (<Redirect to="/products" />);
+    }
+    else{
+        if (product !== undefined) {
+            return (
+                <div className="container">
+                <h3>Edit Products</h3>
+                <form onSubmit = {this.handleSubmit.bind(this)}>
+                    <div className="row">
+                    <input
+                        placeholder="Product Name"
+                        id="productName"
+                        type="text"
+                        name="productName"
+                        value={this.state.productName}
+                        onChange={this.handleChangeName}
+                    />
+                    </div>
+                    <div className="row">
+                    <input
+                        placeholder="Price"
+                        id="price"
+                        type="text"
+                        name="price"
+                        value={this.state.price}
+                        onChange={this.handleChangePrice}
+                    />
+                    </div>
+                    <div className="row">
+                    <input
+                        placeholder="Type"
+                        id="type"
+                        type="text"
+                        name="type"
+                        value={this.state.type}
+                        onChange={this.handleChangeType}
+                    />
+                    </div>
+                    <div className="row" className="displayNone">
+                    <input
+                    disabled 
+                        placeholder="ID"
+                        id="ID"
+                        type="text"
+                        name="type"
+                        value={this.state.id}
+                    />
+                    </div>
+                    <div className="row">
+                    <button
+                        type="submit"
+                        className="waves-effect waves-light btn right-align"
+                        onClick = {this.handleEdit}
+                    >
+                        Add
+                    </button>
+                    </div>
+                </form>
+                </div>
+            );
+            } else {
+            return <h3>No Item</h3>;
+            }
     }
   }
 }
