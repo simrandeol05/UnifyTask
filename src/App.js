ed84d73888db0b2ee1c54981e8ed96735a4e757e
Simran Deol <simrandeol@Simrans-MacBook-Pro.local>
2020-04-22 13:12:31 +0530

    commit changes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div className="App">
+        <div className="App backgroundImg">
         <Navbar />
          <Switch>
          <Route exact path ="/" component={ProductList} />
